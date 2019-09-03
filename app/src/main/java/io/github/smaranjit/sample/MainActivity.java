bd4a99c46d8a9dc76f59aa0fa851795e3e5482cb
Smaranjit Maiti <developer.smaranjit@gmail.com>
2019-09-03 18:58:57 +0530

    fixes many features

--- app/src/main/java/io/github/smaranjit/sample/MainActivity.java
+++ app/src/main/java/io/github/smaranjit/sample/MainActivity.java
@@ -3,12 +3,36 @@ package io.github.smaranjit.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.github.smaranjit.vibenplaylib.VibeNPlay;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mStartButton, mStopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mStartButton = findViewById(R.id.buttonStart);
+        mStopButton = findViewById(R.id.buttonStop);
+
+        VibeNPlay.with(this).sound(true).vibration(true).initialize();
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                VibeNPlay.start();
+            }
+        });
+
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                VibeNPlay.stop();
+            }
+        });
     }
 }
--- vibenplaylib/src/main/java/io/github/smaranjit/vibenplaylib/VibeNPlay.java
+++ vibenplaylib/src/main/java/io/github/smaranjit/vibenplaylib/VibeNPlay.java
@@ -7,7 +7,8 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 
 /**
- * controls alarm and vibration
+ * controls sound and vibration
+ * sound types could be ringtone, alarm, notification
  */
 public class VibeNPlay {
     private static MediaPlayer mMediaPlayer;
@@ -16,63 +17,132 @@ public class VibeNPlay {
     private static boolean mIsSoundOn = true;
     private static long mVibratorFirstBeatLength = 300;
     private static long mVibratorRestLength = 250;
-    private static long mVibratorSecondBeatLength =720;
+    private static long mVibratorSecondBeatLength = 720;
     private static int mVibratorLoopCount = 1000;
     private static boolean mIsSoundLoop = true;
+    public static final int SOUND_RINGTONE = RingtoneManager.TYPE_RINGTONE;
+    public static final int SOUND_ALARM = RingtoneManager.TYPE_ALARM;
+    public static final int SOUND_NOTIFICATION = RingtoneManager.TYPE_NOTIFICATION;
+    public static final int SOUND_ALL = RingtoneManager.TYPE_ALL;
+    private static int SOUND_TYPE = SOUND_RINGTONE;
 
     /**
-     * starts playing alarm and vibration
-     * @param ctx pass the context
+     * initialize VibeNPlay
+     *
+     * @param ctx Context reference
      */
-    public static VibeNPlay with(Context ctx){
+    public static VibeNPlay with(Context ctx) {
         mCtx = ctx;
         return new VibeNPlay();
     }
 
-    public VibeNPlay vibration(boolean isOn){
+    /**
+     * vibration setting
+     *
+     * @param isOn true for on or false for off
+     */
+    public VibeNPlay vibration(boolean isOn) {
         mIsVibrationOn = isOn;
         return this;
     }
 
-    public VibeNPlay sound(boolean isOn){
+    /**
+     * sound setting
+     *
+     * @param isOn true for on or false for off
+     */
+    public VibeNPlay sound(boolean isOn) {
         mIsSoundOn = isOn;
         return this;
     }
 
-    public static void vibrationPattern(long firstbeat, long rest, long secondbeat, int loop){
+    /**
+     * play immediately
+     */
+    public void play() {
+        start();
+    }
+
+    /**
+     * if play not called then call this
+     */
+    public void initialize() {
+        //
+    }
+
+    /**
+     * set pattern of vibration
+     *
+     * @param firstbeat  in millisecond first beat
+     * @param rest       in millisecond rest or pause
+     * @param secondbeat in millisecond second beat
+     * @param loop       no of times to play vibration pattern
+     */
+    public static void setVibrationPattern(long firstbeat, long rest, long secondbeat, int loop) {
         mVibratorFirstBeatLength = firstbeat;
         mVibratorRestLength = rest;
         mVibratorSecondBeatLength = secondbeat;
         mVibratorLoopCount = loop;
     }
 
-    public static void soundPattern(boolean isloop){
+    /**
+     * set sound pattern
+     *
+     * @param isloop play sound in loop or one time
+     */
+    public static void setSoundPattern(boolean isloop) {
         mIsSoundLoop = isloop;
     }
 
-    public static void setVibration(boolean isOn){
+    /**
+     * set sound type
+     *
+     * @param soundType VibeNPlay.SOUND_RINGTONE,VibeNPlay.SOUND_ALARM,VibeNPlay.SOUND_NOTIFICATION,VibeNPlay.SOUND_ALL
+     */
+    public static void setSoundType(int soundType) {
+        SOUND_TYPE = soundType;
+    }
+
+    /**
+     * vibration setting
+     *
+     * @param isOn true for on or false for off
+     */
+    public static void setVibration(boolean isOn) {
         mIsVibrationOn = isOn;
     }
 
-    public static void setSound(boolean isOn){
+    /**
+     * sound setting
+     *
+     * @param isOn true for on or false for off
+     */
+    public static void setSound(boolean isOn) {
         mIsSoundOn = isOn;
     }
 
-    public static void reset(){
+    /**
+     * resets all settings to their default
+     */
+    public static void reset() {
         mIsVibrationOn = true;
         mIsSoundOn = true;
         mVibratorFirstBeatLength = 300;
         mVibratorRestLength = 250;
-        mVibratorSecondBeatLength =720;
+        mVibratorSecondBeatLength = 720;
         mVibratorLoopCount = 1000;
         mIsSoundLoop = true;
+        SOUND_TYPE = SOUND_RINGTONE;
     }
 
-    public static void start(Context context){
-        if (context!=null){
-            if(mIsVibrationOn){
+    /**
+     * starts playing vibration and sound based on their settings
+     */
+    public static void start() {
+        if (mCtx != null) {
+            if (mIsVibrationOn) {
                 try {
-                    Rumble.init(context);
+                    Rumble.init(mCtx);
                     Rumble.makePattern()
                             .beat(mVibratorFirstBeatLength)
                             .rest(mVibratorRestLength)
@@ -83,19 +153,19 @@ public class VibeNPlay {
                 }
             }
 
-            if(mIsSoundOn){
+            if (mIsSoundOn) {
                 try {
-                    Uri alert =  RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+                    Uri alert = RingtoneManager.getDefaultUri(SOUND_TYPE);
                     mMediaPlayer = new MediaPlayer();
-                    mMediaPlayer.setDataSource(context, alert);
-                    final AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                    if (audioManager.getStreamVolume(AudioManager.STREAM_RING) != 0) {
-                        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_RING);
-                        mMediaPlayer.setLooping(mIsSoundLoop);
-                        mMediaPlayer.prepare();
-                        mMediaPlayer.start();
-                    }
-                } catch(Exception e) {
+                    mMediaPlayer.setDataSource(mCtx, alert);
+                    //final AudioManager audioManager = (AudioManager) mCtx.getSystemService(Context.AUDIO_SERVICE);
+                    //if (audioManager.getStreamVolume(AudioManager.STREAM_RING) != 0) {
+                    mMediaPlayer.setAudioStreamType(AudioManager.STREAM_RING);
+                    mMediaPlayer.setLooping(mIsSoundLoop);
+                    mMediaPlayer.prepare();
+                    mMediaPlayer.start();
+                    //}
+                } catch (Exception e) {
                 }
             }
         }
@@ -103,9 +173,9 @@ public class VibeNPlay {
     }
 
     /**
-     * stops playing alarm and vibration
+     * stops playing sounds and vibration
      */
-    public static void stop(){
+    public static void stop() {
         try {
             Rumble.stop();
             mMediaPlayer.stop();
